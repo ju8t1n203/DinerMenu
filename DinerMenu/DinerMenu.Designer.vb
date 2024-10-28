89ba7a1bc314afa00b0f1b06332e9e9cf4f4a907
Justin Bell <Juxin203@gmail.com>
2024-10-27 21:48:03 -0600

    Add project files.

---
+++ DinerMenu/DinerMenu.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DinerMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Old English Text MT", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(61, 9)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(246, 38)
+        Me.DinerNameLabel.TabIndex = 0
+        Me.DinerNameLabel.Text = "Bellstien's Diner"
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(12, 372)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(81, 66)
+        Me.SoupButton.TabIndex = 1
+        Me.SoupButton.Text = "Sou&p"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(99, 372)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(81, 66)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "&Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(186, 372)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(81, 66)
+        Me.FishButton.TabIndex = 1
+        Me.FishButton.Text = "&Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(273, 372)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(81, 66)
+        Me.ExitButton.TabIndex = 1
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DinerMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(371, 442)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "DinerMenu"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Bellstien's Diner Menu"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ DinerMenu/DinerMenu.vb
@@ -0,0 +1,104 @@
+﻿'Justin Bell
+'RCET0265
+'Fall24
+'Diner Menu
+' link
+Public Class DinerMenu
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+    Private Sub ClearLabels()
+        'removes any exsisting labels so the new ones can be added
+        If Me.Tag IsNot Nothing Then
+            Dim labels() As Label = CType(Me.Tag, Label())
+            For Each lbl In labels
+                Me.Controls.Remove(lbl)
+            Next
+            Me.Tag = Nothing
+        End If
+    End Sub
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        ClearLabels()
+        Dim soupName As New Label
+        Dim soup As New Label
+
+        'soup name
+        soupName.Text = "Stew of Yesterday"
+        soupName.Font = New Font("Haettenschweiler", 16, FontStyle.Regular)
+        soupName.AutoSize = True
+
+        'soup description
+        soup.Text = "This delightful stew contains all perfectly unused ingredients from the day before to create an exciting bundle of flavors!"
+        soup.TextAlign = ContentAlignment.MiddleCenter ' Center align the text
+        soup.Font = New Font("Haettenschweiler", 14, FontStyle.Regular)
+        soup.MaximumSize = New Size(ClientSize.Width - 20, 0) ' Ensure text wraps properly
+        soup.AutoSize = True
+
+        'adds controls
+        Me.Controls.Add(soupName)
+        Me.Controls.Add(soup)
+
+        'location for the labels
+        soupName.Location = New Point((ClientSize.Width - soupName.Width) / 2, ClientSize.Height / 3)
+        soup.Location = New Point((ClientSize.Width - soup.Width) / 2, soupName.Location.Y + soupName.Height + 10)
+
+        Me.Tag = New Label() {soupName, soup}
+
+    End Sub
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        ClearLabels()
+        Dim saladName As New Label
+        Dim salad As New Label
+
+        'salad name
+        saladName.Text = "Royale Caesar Salad"
+        saladName.Font = New Font("Haettenschweiler", 16, FontStyle.Regular)
+        saladName.AutoSize = True
+
+        'salad description
+        salad.Text = "Romaine lettuce, croutons, crushed capers, Parmesan cheese, lemon juice, olive oil, egg yolks, worcestershire sauce, anchovies, dijon mustard, black pepper"
+        salad.TextAlign = ContentAlignment.MiddleCenter ' Center align the text
+        salad.Font = New Font("Haettenschweiler", 14, FontStyle.Regular)
+        salad.MaximumSize = New Size(ClientSize.Width - 20, 0) ' Ensure text wraps properly
+        salad.AutoSize = True
+
+        'adds controls
+        Me.Controls.Add(saladName)
+        Me.Controls.Add(salad)
+
+        'location for the labels
+        saladName.Location = New Point((ClientSize.Width - saladName.Width) / 2, ClientSize.Height / 3)
+        salad.Location = New Point((ClientSize.Width - salad.Width) / 2, saladName.Location.Y + saladName.Height + 10)
+
+        Me.Tag = New Label() {saladName, salad}
+
+    End Sub
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        ClearLabels()
+        Dim fishName As New Label
+        Dim fish As New Label
+
+        'fish name
+        fishName.Text = "Tsunami Tuna"
+        fishName.Font = New Font("Haettenschweiler", 16, FontStyle.Regular)
+        fishName.AutoSize = True
+
+        'fish description
+        fish.Text = "This wonderful wild caught tuna is prepared with butter, lemon and those tsunami spices to create a life changing dish."
+        fish.TextAlign = ContentAlignment.MiddleCenter ' Center align the text
+        fish.Font = New Font("Haettenschweiler", 14, FontStyle.Regular)
+        fish.MaximumSize = New Size(ClientSize.Width - 20, 0) ' Ensure text wraps properly
+        fish.AutoSize = True
+
+        'adds controls
+        Me.Controls.Add(fishName)
+        Me.Controls.Add(fish)
+
+        'location for the labels
+        fishName.Location = New Point((ClientSize.Width - fishName.Width) / 2, ClientSize.Height / 3)
+        fish.Location = New Point((ClientSize.Width - fish.Width) / 2, fishName.Location.Y + fishName.Height + 10)
+
+        Me.Tag = New Label() {fishName, fish}
+
+    End Sub
+End Class
